f070a30c85e89ddd705d64bd757f015ff2e77dee
JamesAChavesForero <javeschames2024@gmail.com>
2024-06-20 12:52:17 -0500

    visual MVP

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-let currentView = 1;
 const regExpObject = {
   fName: "^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[s]*)+$",
   lName: "^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[s]*)+$",
   phone: "^[0-9()-]+$",
   email: "[a-zA-Z0-9._-]+@[a-zA-Z]+.+com",
-  address: "^[a-zA-Z0-9s,'-]*$",
+  address: "[A-Za-z0-9'.-s,]",
   zCode: "^[0-9]{5}(?:-[0-9]{4})?$",
   city: "^[a-zA-Z',.s-]{1,25}$",
   state: "^[a-zA-Z',.s-]{1,25}$",
   country: "^[a-zA-Z',.s-]{1,25}$",
 };
-
+let currentView = 1;
+let shippingArray = [];
 let error = (input) => {
   input.classList.add("error");
   input.nextElementSibling.innerHTML = `<img src="./icon-error.svg"/ class="errorImg"> ${input.placeholder} is Invalid`;
   input.nextElementSibling.style.display = "flex";
   document.getElementById("next").disabled = true;
 };
-
 let approved = (input) => {
   input.classList.remove("error");
   input.nextElementSibling.style.display = "none";
   document.getElementById("next").disabled = Array.from(
     document.querySelectorAll("#formContainer .errorText")
-  ).some( errMsg => errMsg.style.display === "flex");
+  ).some((errMsg) => errMsg.style.display === "flex");
 };
-
 document.getElementById("next").addEventListener("click", () => {
   if (currentView === 1) {
-    document.querySelector(".active").classList.remove("active")
-    document.querySelector("li#step2 aside.bar").classList.add("active")
+    document.querySelector(".active").classList.remove("active");
+    document.querySelector("li#step2").classList.add("active");
+    document.querySelector("#formHeader h4").innerText =
+      "Please fill out  Shipping Information ";
+    document.querySelector("#formHeader img").src = "./2.svg";
     document.getElementById("viewSwitch").innerHTML = `<div class="fields">
                 <div>
                     <label for="fName">First name:</label>
@@ -95,8 +96,110 @@ document.getElementById("next").addEventListener("click", () => {
       });
     });
   } else if (currentView === 2) {
-    document.querySelector(".active").classList.remove("active")
-    document.querySelector("li#step3 aside.bar").classList.add("active")
+    document.querySelector(".active").classList.remove("active");
+    document.querySelector("li#step3").classList.add("active");
+    Array.from(document.querySelectorAll("input")).forEach((input) => {
+      input.value != "" && shippingArray.push(`${input.value}`);
+    });
+    document.querySelector("#formHeader h4").innerText = "Review Quote Details";
+    document.querySelector("#formHeader img").src = "./Frame 23.svg";
+    document.querySelector("#viewSwitch").innerHTML = `
+    <h6>Items</h6> 
+    <img src="./img.jpg" style="width: 100%;">
+    <h6>Destination</h6>
+    <ul>
+    ${shippingArray.join(" <br>")}
+    </ul>`;
+    document.getElementById("next").innerText = "Confirm"
+    currentView++;
+  } else if (currentView === 3) {
     document.getElementById("formContainer").classList.add("confirmation");
+    document.querySelector("#viewSwitch").innerHTML = `<h3>
+    Thank you ${shippingArray[0]} <br>
+    You are a step closer to gething exclusive rates <br>
+    We will be in contact with you within 72 hours with a quote
+    </h3>
+    <img src="./cmTruck.svg" class="cmTruck"/>`;
+     document.getElementById("back").style.display = "none"
+      document.getElementById("next").innerText = "Close"
+     document.getElementById("steps").style.display = "none"
+    currentView++;
+  }
+});
+
+document.getElementById("back").addEventListener("click", () => {
+  if (currentView === 2) {
+    document.querySelector(".active").classList.remove("active");
+    document.querySelector("li#step1").classList.add("active");
+    document.querySelector("#formHeader h4").innerText =
+      "Choose items you would like to include in your quote";
+    document.querySelector("#formHeader img").src = "./one.svg";
+    document.getElementById("back").innerText = "Continue Shopping";
+    document.getElementById(
+      "viewSwitch"
+    ).innerHTML = `<img src="./img.jpg" style="width: 100%;">`;
+    currentView--;
+  } else if (currentView === 3){
+    document.querySelector(".active").classList.remove("active");
+    document.querySelector("li#step2").classList.add("active");
+    document.querySelector("#formHeader h4").innerText = "Please fill out Shipping Information";
+    document.querySelector("#formHeader img").src = "./2.svg";
+    document.querySelector("#viewSwitch").innerHTML = `<div class="fields">
+                <div>
+                    <label for="fName">First name:</label>
+                    <input type="text" id="fname" name="fName" placeholder="First name">
+                    <span class="errorText" style="display: none"></span>
+                </div>
+                <div>
+                    <label for="lName">Last name:</label>
+                    <input type="text" id="lName" name="lName" placeholder="Last name">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+            </div>
+            <div class="fields">
+                <div>
+                    <label for="phone">Phone Number</label>
+                    <input type="tel" id="phone" name="phone" placeholder="Phone Number">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Email Address">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+            </div>
+            <div class="fields">
+                <div>
+                    <label for="address"> Shipping Address</label>
+                    <input type="text" id="address" name="address" placeholder="Address">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+            </div>
+            <div class="fields">
+                <div>
+                    <label for="zCode">Zip Code</label>
+                    <input type="number" id="zCode" name="zCode" placeholder="Zip Code ">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+                <div>
+                    <label for="city"> City</label>
+                    <input type="text" id="city" name="city" placeholder="City ">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+            </div>
+            <div class="fields">
+                <div>
+                    <label for="state">State </label>
+                    <input type="text" id="state" name="state" placeholder="State ">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+                <div>
+                    <label for="country">Country </label>
+                    <input type="text" id="country" name="country" placeholder="Country ">
+               <span class="errorText" style="display: none"></span>
+                    </div>
+            </div>`
+           
+    currentView--
   }
 });
